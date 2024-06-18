c25e472b3418293d62345a2012b02e1e792e3739
Ernesto Torralba <e.torralba.ba@gmail.com>
2024-06-18 10:39:18 -0600

    ✨ feat: add eslint to api

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,8 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  req;
-  res.send('Hello World from Express and TypeScript!');
+       req;
+    res.send('Hello World from Express and TypeScript!');
 });
 
 app.listen(PORT, () => {
